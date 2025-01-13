a7f43d4bc31c58901a73730c21419880428d256e
MEETRATHOD006 <109798648+MEETRATHOD006@users.noreply.github.com>
2025-01-13 11:46:40 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 // Import Socket.IO client
 const socket = io("https://watch-togethertest.onrender.com"); // Update the URL as per your server
-import Peer from "peerjs";
+import { Peer } from "peerjs"
 const peer = new Peer();
 
 const peers = {}; // Store peer connections
