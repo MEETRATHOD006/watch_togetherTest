0a5427ffd52ba6ff5d0d87b206611ac8d3e4dec0
MEETRATHOD006 <109798648+MEETRATHOD006@users.noreply.github.com>
2025-01-14 17:16:50 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -67,7 +67,7 @@ if (roomId) {
     });
   })
 
-  socket.on('user-disconnected', (roomId, userId) => {
+  socket.on('user-disconnected', userId => {
     if (peers[userId]) peers[userId].close()
   })
 
